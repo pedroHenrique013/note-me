27d5ad684a3704dc5c7917cb7ea24e43c6c6eafe
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 11:10:39 -0300

    feat: Arquivo de estilização do componente AppContent

---
+++ src/page/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AppContent = styled.div`
+    display: grid;
+    grid-template-columns: 112px auto;
+    grid-template-rows: auto 1fr;
+    grid-gap: 0 112px;
+`;
