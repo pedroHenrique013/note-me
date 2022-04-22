703cd43e4b194b1fa5464e3451874125edd07d05
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:23:13 -0300

    feat: Criação do componente Greeting

---
+++ src/components/Greeting/index.jsx
@@ -0,0 +1,12 @@
+import {GreetingContent} from './styles';
+
+export function Greeting() {
+  return (
+    <GreetingContent>
+      <h1>
+        <span>Olá,</span> Pedro!👋
+      </h1>
+      <p>Todas as suas notas estão aqui, em um só lugar!</p>
+    </GreetingContent>
+  );
+}
