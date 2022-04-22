4f0bab2ac963f508f39133c1f7badccfa245538b
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 15:37:17 -0300

    feat: NoteCard adicionado no componente Main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,12 @@
 import { Greeting } from '../Greeting';
+import { NoteCard } from '../NoteCard';
 import {MainContent} from './styles';
 
 export function Main() {
     return (
         <MainContent>
             <Greeting />
+            <NoteCard />
         </MainContent>
     );
 }
