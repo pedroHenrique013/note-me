0fb696f7cb9dff296cb6e1d98acc40327e2966f8
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:24:52 -0300

    feat: Arquivo de estilização do componente Greeting

---
+++ src/components/Greeting/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GreetingContent = styled.div`
+  
+  
+  
+  h1 {
+    color: var(--primary-white);
+  }
+
+  p {
+    color: var(--gray-300);
+    margin-top: 12px;
+  }
+`;
