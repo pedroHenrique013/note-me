e8b25779964574ad72b81ed6f309595c092dc44d
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:27:27 -0300

    feat: Main adicionado no componente principal App

--- src/page/Home/App.jsx
+++ src/page/Home/App.jsx
@@ -2,6 +2,7 @@ import {AppContent} from './styles'
 import { GlobalStyle} from '../../styles/global'
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/SideBar'
+import { Main } from '../../components/Main'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <AppContent>
       <Header />
       <Sidebar />
+      <Main />
       <GlobalStyle />
     </AppContent>
   )
