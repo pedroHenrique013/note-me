1e26c06906464abd8e945ad0202d59583fc91254
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:17:21 -0300

    feat: Arquivo de estilização do componente Main

---
+++ src/components/Main/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainContent = styled.main`
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 3;
+
+`;
