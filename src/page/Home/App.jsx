7785cbcf61110840cc75a4bb147b95e237c5739c
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:02:22 -0300

    feat: Header, SideBar adicionado no componente principal App

--- src/page/Home/App.jsx
+++ src/page/Home/App.jsx
@@ -1,10 +1,14 @@
 import {AppContent} from './styles'
 import { GlobalStyle} from '../../styles/global'
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/SideBar'
 
 function App() {
 
   return (
     <AppContent>
+      <Header />
+      <Sidebar />
       <GlobalStyle />
     </AppContent>
   )
