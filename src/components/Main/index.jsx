ef4d7db3d557cc75c33ff33faf0c341a426d5e84
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-22 14:26:21 -0300

    feat: Greeting adicionado no componente Main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,10 @@
+import { Greeting } from '../Greeting';
 import {MainContent} from './styles';
 
 export function Main() {
     return (
         <MainContent>
-
+            <Greeting />
         </MainContent>
     );
 }
