33b56fb5cadf39660821e5ced99e7e60b2341e64
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-24 20:08:42 -0300

    feat: Função de adicionar notas passada como propriedade para o componente e para o botão

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -2,12 +2,12 @@ import logoImg from '../../assets/logo.png';
 import addImg from '../../assets/reanking.svg';
 import {SideBarContainer, SideBarContent} from './styles';
 
-export function Sidebar() {
+export function Sidebar({addNotes}) {
   return (
     <SideBarContainer>
       <SideBarContent>
         <img src={logoImg} alt="Logo de um lapís escrevendo" />
-        <button>
+        <button onClick={addNotes}>
           <img src={addImg} alt="Icone de criaçao de notas" />
         </button>
       </SideBarContent>
