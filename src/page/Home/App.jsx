362c31f90270604b7c28e950d9c9d03b07f2db4d
pedroHenrique013 <henrique.netsto@gmail.com>
2022-04-24 20:05:37 -0300

    feat: Implementado as funcionalidades de adicionar uma nota e excluir

--- src/page/Home/App.jsx
+++ src/page/Home/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {AppContent} from './styles'
 import { GlobalStyle} from '../../styles/global'
 import { Header } from '../../components/Header'
@@ -5,12 +6,31 @@ import { Sidebar } from '../../components/SideBar'
 import { Main } from '../../components/Main'
 
 function App() {
+const [notes, setNotes] = useState([
+  {
+    id: 1,
+    content: 'Note 1',
+  }
+]);
+
+function handleAddNote() {
+  const newNote = {id: notes.length + 1, content: 'New Note'};
+  setNotes([...notes, newNote]);
+
+}
+
+function handleDeleteNote(deleteNote) {
+  console.log(notes)
+  console.log(deleteNote);
+  const filterNote = notes.filter(note => note.id !== deleteNote);
+  setNotes(filterNote);
+}
 
   return (
     <AppContent>
       <Header />
-      <Sidebar />
-      <Main />
+      <Sidebar addNotes={handleAddNote} />
+      <Main notes={notes} removeNotes={handleDeleteNote} />
       <GlobalStyle />
     </AppContent>
   )
